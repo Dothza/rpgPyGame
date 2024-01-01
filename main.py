3cd4098ef1e5d526bc6bf3aed8fb71bbf7aedfbe
vzffelnzes <104564290+vzffelnzes@users.noreply.github.com>
2024-01-01 16:27:12 +0300

    add main

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import pygame
 import sys
 
+SIZE_WINDOW = (800, 600)
+
 
 def terminate():
     pygame.quit()
     sys.exit()
+
+
+def main():
+    screen = pygame.display.set_mode(SIZE_WINDOW)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
